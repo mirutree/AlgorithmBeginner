c30b83e6af9383b0659a87f51b2c1830e1c6f283
mirutree <wonyiwonyi@naver.com>
2021-12-15 16:15:53 +0900

    복습

--- src/chap03/seqSearch.java
+++ src/chap03/seqSearch.java
@@ -9,6 +9,16 @@ public class seqSearch {
         int i = 0;
 
         while (true) {
+            /*
+                선형 검색에서 ( 맨 앞부터 순서대로 요소 검색)의 종료조건은
+                1. 검색 실패하거나
+                2. 검색 성공하거나 인데,
+                실패하는 경우는 배열 마지막까지 가서야 없는 경우이므로 배열 크기만큼 돌아야 종료된다
+                i가 0부터 시작해서 하나씩 올라가니까 n번째까지 반복했다는 것은 찾는 값이 없다(실패했다)라는 뜻
+                i == n이 성립하는 경우 (종료조건 1 : 검색 실패이므로 -1를 반환)
+                그만큼 반복되지 않았다면 (i++해서 i == n이 되기 전에 종료되었다면) 검색이 성공했다는 뜻이고
+                a[i]에 그 키 값이 있다.
+             */
             if (i==n)
                 return -1; // 검색 실패하면 리턴 -1
             if (a[i] == key)
@@ -24,6 +34,7 @@ public class seqSearch {
         int num = stdIn.nextInt();
         int[] x = new int[num];		// 요솟수가 num인 배열
 
+        // 요소수를 입력받는 포문
         for (int i = 0; i < num; i++) {
             System.out.print("x[" + i + "]：");
             x[i] = stdIn.nextInt();
@@ -34,7 +45,7 @@ public class seqSearch {
 
         int idx = seqSearch(x, num, ky);	// 배열x에서 키 값이 ky인 요소를 검색
 
-        if (idx == -1)
+        if (idx == -1) // 검색 실패
             System.out.println("그 값의 요소가 없습니다.");
         else
             System.out.println(ky+"은(는) x[" + idx + "]에 있습니다.");
